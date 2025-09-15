8a4b8fbfaee1e7f4d92b02821e6d4428e5a3225d
Lander-Janssens <landerjanssens9@gmail.Com>
2025-09-15 13:40:48 +0200

    Opdracht 01.01 is klaar

--- 01/01_01/consoleapp/Program.cs
+++ 01/01_01/consoleapp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Declaratie variabelen
+int getal1;
+int getal2;
+int getal3;
+int getal4;
+int optelling, vermenigvuldiging;
+
+Console.WriteLine("Geef uw eerste getal voor de optelling.");
+getal1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Geef uw tweede getal voor de optelling.");
+getal2 = int.Parse(Console.ReadLine());
+
+optelling = (getal1 + getal2);
+
+Console.WriteLine($"Optelling: {optelling}");
+
+Console.WriteLine("Geef uw eerste getal voor de vermenigvuldiging");
+getal3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Geef uw tweede getal voor de vermenigvuldiging");
+getal4 = int.Parse(Console.ReadLine());
+
+vermenigvuldiging = (getal3 * getal4);
+
+Console.WriteLine($"Vermenigvuldiging: {vermenigvuldiging}");
