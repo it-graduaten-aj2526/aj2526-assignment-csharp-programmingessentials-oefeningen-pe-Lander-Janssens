b70468fa3028b80e07b2858cb6a37efd7c76b996
Lander-Janssens <landerjanssens9@gmail.Com>
2025-09-15 20:06:41 +0200

    Oefening 01_02 is klaar!

--- 01/01_02/consoleapp/Program.cs
+++ 01/01_02/consoleapp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Declaratie variabelen
+double prijs1;
+double BTW;
+
+
+Console.WriteLine("Geef de prijs in");
+prijs1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Geef de BTW in"); 
+BTW = double.Parse(Console.ReadLine());
+
+prijs1 += (prijs1 * (BTW / 100) );
+
+Console.WriteLine($"Prijs inclusief btw is {Convert.ToInt32(prijs1)}");
