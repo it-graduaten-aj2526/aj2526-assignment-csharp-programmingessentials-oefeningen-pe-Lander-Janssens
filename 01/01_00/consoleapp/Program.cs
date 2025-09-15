e4cab0e333297105a1153747a4afec28812b62ed
Lander-Janssens <landerjanssens9@gmail.Com>
2025-09-15 11:31:13 +0200

    Oef 01.00 is klaar

--- 01/01_00/consoleapp/Program.cs
+++ 01/01_00/consoleapp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Declaratie variabelen
+int kortezijde;
+int langezijde;
+int omtrek, oppervlakte;
+
+Console.WriteLine("Geef de korte zijde: ");
+kortezijde = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Geef de lange zijde: ");
+langezijde = int.Parse(Console.ReadLine());
+
+omtrek = (kortezijde + langezijde);
+oppervlakte = kortezijde * langezijde;
+
+Console.WriteLine($"Omtrek: {omtrek}");
+Console.WriteLine($"Oppervlakte: {oppervlakte}");
