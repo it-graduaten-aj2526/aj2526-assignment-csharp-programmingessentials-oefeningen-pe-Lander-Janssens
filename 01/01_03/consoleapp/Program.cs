31a97626b7c1553b742f5d1bad3f9afdba75ff24
Lander-Janssens <landerjanssens9@gmail.Com>
2025-09-15 21:03:55 +0200

    Opdrach 01_03 is klaar!

--- 01/01_03/consoleapp/Program.cs
+++ 01/01_03/consoleapp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region variabelen
+double prijs1 = 10;
+double prijs2 = 7.50;
+double volwassenen, kinderen;
+#endregion
+
+Console.WriteLine("Met hoeveel volwassenen zijn jullie?");
+volwassenen = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Met hoeveel kinderen zijn jullie?");
+kinderen = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Het totale bedrag is: {Convert.ToInt32((prijs1 * volwassenen) + (prijs2 * kinderen))}");
