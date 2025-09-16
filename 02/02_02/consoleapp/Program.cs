04ffc4f66762825e0554db36a07319ba57d2f1e0
Lander-Janssens <landerjanssens9@gmail.Com>
2025-09-16 10:48:06 +0200

    Oefening 02_02 is klaar

--- 02/02_02/consoleapp/Program.cs
+++ 02/02_02/consoleapp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int getal1, getal2;
+
+Console.Write("Geef je eerste getal in");
+getal1 = int.Parse(Console.ReadLine());
+
+Console.Write("Geef je tweede getal in");
+getal2 = int.Parse(Console.ReadLine());
+
+if (getal2 == 0)
+{
+    Console.Write("Is niet deelbaar door nul");
+}
+else
+{
+    getal1 = getal1 / getal2;
+    Console.WriteLine($"de uitkomst is {getal1}");
+}
