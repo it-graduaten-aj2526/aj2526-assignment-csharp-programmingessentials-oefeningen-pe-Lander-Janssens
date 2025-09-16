d5aa4572c0b1057a8d0287a1c6b90e3a6f7c69e7
Lander-Janssens <landerjanssens9@gmail.com>
2025-09-16 08:52:04 +0200

    Update Program.cs

--- 01/01_01/consoleapp/Program.cs
+++ 01/01_01/consoleapp/Program.cs
@@ -1,9 +1,5 @@
 ﻿//Declaratie variabelen
-int getal1;
-int getal2;
-int getal3;
-int getal4;
-int optelling, vermenigvuldiging;
+int getal1, getal2, getal3, getal4, optelling, vermenigvuldiging;
 
 Console.WriteLine("Geef uw eerste getal voor de optelling.");
 getal1 = int.Parse(Console.ReadLine());
@@ -11,16 +7,14 @@ getal1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Geef uw tweede getal voor de optelling.");
 getal2 = int.Parse(Console.ReadLine());
 
-optelling = (getal1 + getal2);
-
-Console.WriteLine($"Optelling: {optelling}");
-
 Console.WriteLine("Geef uw eerste getal voor de vermenigvuldiging");
 getal3 = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Geef uw tweede getal voor de vermenigvuldiging");
 getal4 = int.Parse(Console.ReadLine());
 
-vermenigvuldiging = (getal3 * getal4);
+optelling = (getal1 + getal3);
+vermenigvuldiging = (getal2 * getal4);
 
+Console.WriteLine($"Optelling: {optelling}");
 Console.WriteLine($"Vermenigvuldiging: {vermenigvuldiging}");
