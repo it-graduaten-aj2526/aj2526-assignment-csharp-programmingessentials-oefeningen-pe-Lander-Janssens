4ab9c5c3fe807f3afb7a435ce17e22877fe62c9b
Lander-Janssens <landerjanssens9@gmail.Com>
2025-09-16 10:07:05 +0200

    Oefening 02_01 is klaar

--- 02/02_01/consoleapp/Program.cs
+++ 02/02_01/consoleapp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int getal1, getal2;
+
+Console.Write("Voer het eerste getal in: ");
+getal1 = int.Parse(Console.ReadLine());
+
+Console.Write("Voer het tweede getal in: ");
+getal2 = int.Parse(Console.ReadLine());
+
+if (getal1 >= getal2)
+{
+    getal1 = getal1 - getal2;
+}
+else
+{
+    getal1 = getal2 - getal1;
+}
+Console.WriteLine($"De uitkomst is {getal1}");
