81214f8c208b5058bd558908545cdf6c6dcf4513
Lander-Janssens <landerjanssens9@gmail.com>
2025-09-17 13:27:33 +0200

    Update Program.cs

--- 01/01_03/consoleapp/Program.cs
+++ 01/01_03/consoleapp/Program.cs
@@ -3,9 +3,9 @@ double prijs2 = 7.50;
 int volwassenen, kinderen;
 
 Console.WriteLine("Met hoeveel volwassenen zijn jullie?");
-volwassenen = double.Parse(Console.ReadLine());
+volwassenen = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Met hoeveel kinderen zijn jullie?");
-kinderen = double.Parse(Console.ReadLine());
+kinderen = int.Parse(Console.ReadLine());
 
 Console.WriteLine($"Het totale bedrag is: {Convert.ToInt32((prijs1 * volwassenen) + (prijs2 * kinderen))}");
