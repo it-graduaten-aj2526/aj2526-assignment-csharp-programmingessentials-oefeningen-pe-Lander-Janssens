1dbb17d9352a2db780b14e0178e5d1019fea6093
Lander-Janssens <landerjanssens9@gmail.Com>
2025-09-17 14:49:58 +0200

    Oefening 01_04 is klaar

--- 01/01_04/consoleapp/Program.cs
+++ 01/01_04/consoleapp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int internationale, nationale;
+
+Console.Write("Hoeveel internationale oproepen heeft u gemaakt: ");
+internationale = int.Parse(Console.ReadLine());
+
+Console.Write("Hoeveel nationale oproepen heeft u gemaakt: ");
+nationale = int.Parse(Console.ReadLine());
+
+int totaleGesprekken = internationale + nationale;
+
+double eindprijs = totaleGesprekken * 0.12 + 23 + (totaleGesprekken * 0.12 + 23 ) * 0.21;
+
+Console.WriteLine($"Het totaal te betalen bedrag is: {Convert.ToInt32(eindprijs)}");
