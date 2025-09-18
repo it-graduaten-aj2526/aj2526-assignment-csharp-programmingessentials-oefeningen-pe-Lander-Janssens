aab67b3db293199f767328be5424f584c7380037
Lander-Janssens <landerjanssens9@gmail.com>
2025-09-18 11:25:28 +0200

    Update Program.cs

--- 02/02_02/consoleapp/Program.cs
+++ 02/02_02/consoleapp/Program.cs
@@ -1,16 +1,16 @@
-﻿int getal1, getal2;
+int getal1, getal2;
 
-Console.Write("Geef je eerste getal in: ");
+Console.Write("Geef je eerste getal in");
 getal1 = int.Parse(Console.ReadLine());
 
-Console.Write("Geef je tweede getal in: ");
+Console.Write("Geef je tweede getal in");
 getal2 = int.Parse(Console.ReadLine());
 
 if (getal2 == 0)
 {
-    Console.Write("Is niet deelbaar door nul");
+    Console.Write($" {getal1} is niet deelbaar door 0");
 }
 else
 {
-    Console.WriteLine($"{getal1} / {getal2} = {getal1 = getal1 / getal2}");
+    Console.WriteLine($"de uitkomst is {getal1 = getal1 / getal2}");
 }
