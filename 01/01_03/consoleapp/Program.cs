430e4dc5395945157bdd6971f325e7b5f06226e9
Lander-Janssens <landerjanssens9@gmail.Com>
2025-09-19 14:42:14 +0200

    update!

--- 01/01_03/consoleapp/Program.cs
+++ 01/01_03/consoleapp/Program.cs
@@ -1,10 +1,10 @@
 
 int volwassenen, kinderen;
 
-Console.Write("Met hoeveel volwassenen zijn jullie?:");
+Console.Write("Met hoeveel volwassenen zijn jullie?: ");
 volwassenen = int.Parse(Console.ReadLine());
 
-Console.Write("Met hoeveel kinderen zijn jullie?:");
+Console.Write("Met hoeveel kinderen zijn jullie?: ");
 kinderen = int.Parse(Console.ReadLine());
 
 Console.WriteLine($"Totaal te betalen: {Convert.ToInt32((10 * volwassenen) + (7,50 * kinderen))}");
